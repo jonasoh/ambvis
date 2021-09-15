0ad1a240fc90673268bd57953bbd6bf1cbe96fed
Jonas Ohlsson <jonas@vansinne.se>
2021-09-15 09:33:16 +0200

    allow decorating regardless of how module was imported

--- ambvis/decorators.py
+++ ambvis/decorators.py
@@ -3,17 +3,31 @@ import inspect
 from ambvis import globals
 
 
+def removeprefix(s, prefix):
+    s = str(s)
+    if s.startswith(prefix):
+        return s[len(prefix):]
+    else:
+        return s[:]
+
+
 def public_route(f):
     '''decorator for routes that should be accessible without being logged in'''
-    caller = inspect.currentframe().f_back
     globals.public_routes.add(f.__name__)
-    globals.public_routes.add(caller.f_globals['__name__'] + '.' + f.__name__)
+
+    caller = inspect.currentframe().f_back
+    calling_module = caller.f_globals['__name__']
+    calling_module = removeprefix(calling_module, 'ambvis.')
+    globals.public_routes.add(calling_module + '.' + f.__name__)
     return f
 
 
 def not_while_running(f):
     '''decorator for routes that should be inaccessible while an experiment is running'''
-    caller = inspect.currentframe().f_back
     globals.not_while_running.add(f.__name__)
-    globals.not_while_running.add(caller.f_globals['__name__'] + '.' + f.__name__)
+
+    caller = inspect.currentframe().f_back
+    calling_module = caller.f_globals['__name__']
+    calling_module = removeprefix(calling_module, 'ambvis.')
+    globals.not_while_running.add(calling_module + '.' + f.__name__)
     return f
