124903e3014f1983c42dae28c87e146d50888217
Jonas Ohlsson <jonas@vansinne.se>
2021-10-06 10:51:41 +0200

    use cherrypy's logging facilities

--- ambvis/logger.py
+++ ambvis/logger.py
@@ -3,15 +3,16 @@
 #
 
 import sys
+
+from cherrypy import log as cherrypy_log
+
 from ambvis.config import cfg
 
 
 def log(msg):
-    sys.stderr.write(msg + '\n')
-    sys.stderr.flush()
+    cherrypy_log(msg)
 
 
 def debug(msg):
     if cfg.get('debug'):
-        sys.stderr.write(msg + '\n')
-        sys.stderr.flush()
+        cherrypy_log(msg)
