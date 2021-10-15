b9126d9af76962f7e461ad321cc03816322280d3
Jonas Ohlsson <jonas@vansinne.se>
2021-10-15 09:49:41 +0200

    cleanup and fix AWB

--- ambvis/experimenter.py
+++ ambvis/experimenter.py
@@ -6,11 +6,9 @@ from ambvis.config import cfg
 from ambvis.logger import log, debug
 from ambvis.hw import cam, motor, led
 
+
 class Experimenter(threading.Thread):
     def __init__(self):
-        self.cam = cam
-        self.motor = motor
-        self.led = led
         self.cfg = cfg
         self.dir = os.path.expanduser('~')
         self.imgfreq = 60
@@ -24,7 +22,6 @@ class Experimenter(threading.Thread):
     def run(self):
         while not self.quit:
             self.status_change.wait()
-            log('Status change')
             self.status_change.clear()
             self.run_experiment()
 
@@ -32,32 +29,41 @@ class Experimenter(threading.Thread):
         self.running = True
         os.makedirs(self.dir, exist_ok=True)
         saved_pos = cfg.get('positions')
-        self.cam.streaming = False
+        cam.streaming = False
         log('Starting experiment ' + os.path.basename(self.dir))
 
+        # fix AWB
+        led.on = True
+        cam.camera.awb_mode = "auto"
+        time.sleep(3)
+        g = cam.camera.awb_gains
+        cam.camera.awb_mode = "off"
+        cam.camera.awb_gains = g
+        led.on = False
+
         while not self.stop_experiment:
+            motor.find_home()
             for pos in saved_pos:
                 loopstart = time.time()
-                if self.motor.position != pos: 
-                    self.motor.move(pos)
-                self.led.on = True
+                if motor.position != pos:
+                    motor.move(pos)
+                led.on = True
                 time.sleep(1)
                 now = time.strftime("%Y%m%d-%H%M%S", time.localtime())
                 fname = os.path.join(self.dir, 'pos_' +
                                      str(pos) + '_' + now + '.png')
                 cam.fast_capture(fname)
-                self.led.on = False
-
-            self.motor.move(saved_pos[0])
+                led.on = False
 
             time_taken = time.time() - loopstart
             time_left = self.imgfreq*60 - time_taken
 
             while time_left > 0 and not self.stop_experiment:
-                time.sleep(time_left)
+                time.sleep(1)
                 time_taken = time.time() - loopstart
                 time_left = self.imgfreq*60 - time_taken
 
         log('Experiment finished.')
-        self.cam.streaming = True
+        cam.camera.awb_mode = "auto"
+        cam.streaming = True
         self.running = False
